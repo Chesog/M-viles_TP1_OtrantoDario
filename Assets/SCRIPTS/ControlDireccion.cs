59e8ca2eb998fb54eac872b960ada38df144633d
DarioOtr <otrantodario@gmail.com>
2023-09-24 05:19:24 -0300

    Add Touch Controllers For Conduction

--- Assets/SCRIPTS/ControlDireccion.cs
+++ Assets/SCRIPTS/ControlDireccion.cs
@@ -2,10 +2,8 @@ using UnityEngine;
 
 public class ControlDireccion : MonoBehaviour 
 {
-	public enum TipoInput {AWSD, Arrows}
-	public TipoInput InputAct = TipoInput.AWSD;
-
 	float Giro = 0;
+	public int playerId = 1;
 	
 	public bool Habilitado = true;
 	CarController carController;
@@ -19,38 +17,9 @@ public class ControlDireccion : MonoBehaviour
 	}
 	
 	// Update is called once per frame
-	void Update () 
+	void Update ()
 	{
-		switch(InputAct)
-		{
-            case TipoInput.AWSD:
-                if (Habilitado) {
-                    if (Input.GetKey(KeyCode.A)) {
-						Giro = -1;
-                    }
-                    else if (Input.GetKey(KeyCode.D)) {
-						Giro = 1;
-                    }
-                    else {
-						Giro = 0;
-					}
-                }
-                break;
-            case TipoInput.Arrows:
-                if (Habilitado) {
-                    if (Input.GetKey(KeyCode.LeftArrow)) {
-						Giro = -1;
-					}
-                    else if (Input.GetKey(KeyCode.RightArrow)) {
-						Giro = 1;
-					}
-                    else {
-						Giro = 0;
-					}
-                }
-                break;
-        }
-
+		Giro = InputManager.Instance.GetAxis($"Horizontal{playerId}"); 
 		carController.SetGiro(Giro);
 	}
 
---
+++ Assets/SCRIPTS/NewScripts/InputManager.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputManager
+{
+    private float giro;
+    private Dictionary<string, float> axisValues = new Dictionary<string, float>();
+    private static InputManager instance;
+
+    public static InputManager Instance
+    {
+        get
+        {
+            if (instance == null)
+                instance = new InputManager();
+
+            return instance;
+        }
+    }
+
+    public void SetAxis(string inputName,float value)
+    {
+        if (!axisValues.ContainsKey(inputName))
+            axisValues.Add(inputName,value);
+
+        axisValues[inputName] = value;
+    }
+
+    public float GetOrAddAxis(string inputName)
+    {
+        if (!axisValues.ContainsKey(inputName))
+            axisValues.Add(inputName,0f);
+        return axisValues[inputName];
+    }
+
+    public float GetAxis(string inputName)
+    {
+#if UNITY_EDITOR
+        return GetOrAddAxis(inputName) + Input.GetAxis(inputName);
+#endif
+#if UNITY_ANDROID || UNITY_IOS
+        return GetOrAddAxis(inputName);
+#endif
+#if UNITY_STANDALONE
+        Input.GetAxis(inputName);
+#endif
+    }
+}
